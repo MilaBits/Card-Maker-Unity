c1c2e26207bb7ed895b778166150d3d5dbd5d844
Milly <milabits@gmail.com>
2019-02-01 17:01:00 +0100

    Zoom

---
+++ Assets/Scripts/Zoom.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Zoom : MonoBehaviour {
+
+    private float ZoomLevel;
+    
+    public void ChangeZoom(float value) {
+        transform.localScale = new Vector3(value, value, value);
+    }
+}
