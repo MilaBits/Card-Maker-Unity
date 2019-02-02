e7c3a614aad8418d41ed607f8781aaa2c1c4619c
Milly <milabits@gmail.com>
2019-02-02 23:10:30 +0100

    Zoom better pls pick me

---
+++ Assets/Scripts/Zoomer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Zoomer : MonoBehaviour
+{
+    public void Zoom(Slider slider) {
+        transform.localScale = new Vector3(slider.value, slider.value, slider.value);
+    }
+}
