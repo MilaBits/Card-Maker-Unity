78ee4cd7c8a905487747b54cc0a752784a3bdc56
Milly <milabits@gmail.com>
2019-02-03 18:29:43 +0100

    working on card data

---
+++ Assets/Scripts/CardUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CardUI : MonoBehaviour {
+    public TMP_InputField NameInput;
+    public TMP_InputField SummaryInput;
+    public TMP_InputField DescriptionInput;
+    public TMP_Dropdown PhaseInput;
+    
+    [Space]
+    public TMP_InputField RollTargetInput;
+    public TMP_Dropdown RollTypeInput;
+    public TMP_InputField RollTextInput;
+    
+    [Space]
+    public TMP_Dropdown CriticalEffectTypeInput;
+    public TMP_InputField FirstEffectInput;
+    public TMP_InputField SecondEffectInput;
+    
+    [Space]
+    public TMP_Dropdown MainRoleInput;
+    public TMP_Dropdown SecondRoleInput;
+    public TMP_InputField CostInput;
+
+    [Space]
+    public TMP_InputField ImageInput;
+
+}
---
+++ Assets/Scripts/ScriptableObjects/Card.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class Card : ScriptableObject {
+
+    // Basic Information
+    public string Name;
+    public string Summary;
+    public string Description;
+    public Phase Phase;
+
+    // Rolls
+    public string RollTarget;
+    public RollType RollType;
+    public string RollText;
+    
+    // Critical Effects
+    public CriticalType CriticalType;
+    public string FirstEffect;
+    public string SecondEffect;
+
+    // Crew
+    public Role MainRole;
+    public List<Role> SecondaryRoles;
+    public int Cost;
+
+    // Image
+    public Image Image;
+}
